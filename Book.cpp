05aec3edf7912c07af6e163ceafedb517411b049
Atif <atifranaofficial@gmail.com>
2023-05-02 21:08:00 +0500

    Book class cpp file

---
+++ Book.cpp
@@ -0,0 +1,14 @@
+#include "Book.h"
+#include<iostream>
+#include<string>
+using namespace std;
+
+Book::Book(){}
+
+Book::Book(string isbn, string title, string authors,
+    string publisher, string edition, int year)
+    : isbn_(isbn), title_(title), authors_(authors), publisher_(publisher), edition_(edition), year_(year) {}
+
+
+
+
