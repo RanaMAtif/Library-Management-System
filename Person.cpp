0c06d8ab605adc55405c47d85c576e156bd48a29
Atif <atifranaofficial@gmail.com>
2023-05-02 21:13:46 +0500

    Person class cpp file

---
+++ Person.cpp
@@ -0,0 +1,24 @@
+#include "Person.h"
+#include<string>
+#include<iostream>
+using namespace std;
+
+Person::Person(string name, string email) : name_(name), email_(email) 
+{}
+Person::Person() {}
+void Person::set_name(string name)
+{
+	name_ = name;
+}
+string Person::get_name()
+{
+	return name_;
+}
+string Person::get_email()
+{
+	return email_;
+}
+void Person::set_email(string email)
+{
+	email_ = email;
+}
