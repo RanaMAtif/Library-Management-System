30cfcb882b9d4c2a2958c9f77e7b5aa087967e56
Atif <atifranaofficial@gmail.com>
2023-05-02 21:14:00 +0500

    Person class .h file

---
+++ Person.h
@@ -0,0 +1,20 @@
+#pragma once
+#include<string>
+using namespace std;
+
+
+class Person
+{            
+    public:
+        Person(string name, string email);
+        Person();
+        void set_name(string);
+        string get_name();
+        string get_email();
+        void set_email(string);
+    
+    protected:
+        string name_;
+        string email_;
+};
+
