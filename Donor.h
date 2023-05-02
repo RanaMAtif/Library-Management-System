bf092a593feb0d26a2ed31cd5596723bde6a0bef
Atif <atifranaofficial@gmail.com>
2023-05-02 21:15:25 +0500

    Donor class .h file

---
+++ Donor.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Person.h"
+#include<string>
+using namespace std;
+class Donor : public Person
+{
+private:
+   
+public:
+        Donor();
+        Donor(string name,string email); 
+        
+     
+        
+};
+
