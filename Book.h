1b80fe909f8c2e6a10a021b37bcdf4fc3ce91c29
Atif <atifranaofficial@gmail.com>
2023-05-02 21:09:37 +0500

    Book class .h file

---
+++ Book.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<iostream>
+#include<string>
+
+using namespace std;
+class Book
+{
+public:
+    Book();
+    Book(string isbn, string title, string authors,
+        string publisher, string edition, int year);
+    
+    
+   
+protected:
+    string isbn_;
+    string title_;
+    string authors_;
+    string publisher_;
+    string edition_;
+    int year_;
+};
+
+
