0f1191c7deeb9f3ef885e8b1c50a923e295fd25f
Atif <atifranaofficial@gmail.com>
2023-05-02 21:14:48 +0500

    User class cpp file

---
+++ User.cpp
@@ -0,0 +1,14 @@
+#include "User.h"
+#include"Person.h"
+#include<iostream>
+#include <string>
+
+using namespace std;
+User::User() {}
+User::User(string name, string email) : Person(name, email) {}
+
+
+
+
+
+ 
